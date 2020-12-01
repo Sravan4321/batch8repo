14217cd2c7de70aa2a913fc97d207a871f2d6156
root <root@ip-172-31-44-90.ap-south-1.compute.internal>
2020-12-01 02:13:45 +0000

    part of batch8learning

---
+++ home.java
@@ -0,0 +1,4 @@
+Public class home{
+	public static void main(String []args){
+	System.out.println("Welcome to Home")}
+		}
---
+++ login.java
@@ -0,0 +1,4 @@
+public class Login{
+	public static void main([]args){
+	System.out.println("Login to web"){
+		}
